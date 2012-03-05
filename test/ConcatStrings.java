020aabddef204befacc7406bf56a90cfe1712eea
CJ Carey <perimosocordiae@gmail.com>
2012-03-05 17:17:15 -0500

    adding string concat testcase

---
+++ test/ConcatStrings.java
@@ -0,0 +1,9 @@
+// concatenate strings
+public class ConcatStrings {
+  public static void main(String[] args) {
+    String a = "hello";
+    String b = "world";
+    String c = a + " " + b;
+    String d = "" + 7 + 5.43 + -2L + 3.14f + "\n";
+  }
+}
