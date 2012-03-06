adec23d172d63738c482bcec64b3a242b20b1cc5
CJ Carey <perimosocordiae@gmail.com>
2012-03-06 11:28:42 -0500

    adding hashmap test case, passes both disas and runtime tests

---
+++ test/HashMapOps.java
@@ -0,0 +1,12 @@
+// test basic hash map operations
+import java.util.HashMap;
+public class HashMapOps {
+  public static void main(String[] args) {
+    HashMap<String,Integer> foo = new HashMap<String,Integer>();
+    foo.put("hello",5);
+    foo.put("world",5);
+    foo.put("hello",604);
+    int a = foo.get("hello");
+    int b = foo.get("world");
+  }
+}
