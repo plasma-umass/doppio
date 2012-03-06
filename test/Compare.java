58d36c558e07c42285567010c6b26fd103294201
CJ Carey <perimosocordiae@gmail.com>
2012-03-06 17:26:08 -0500

    adding another disas-breaking test case

---
+++ test/Compare.java
@@ -0,0 +1,10 @@
+public class Compare {
+  public static void main(String[] args) {
+  	long foo = System.currentTimeMillis();
+    if (foo < 0L) {
+    	int a = 10000;
+    } else {
+    	int a = 99999;
+    }
+  }
+}
