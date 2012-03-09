3cb36eee044655f8c3a70cfd1dde1d8880a7c672
Jez Ng <jezreel@gmail.com>
2012-03-09 16:00:18 -0500

    Make selection of tests more granular.

--- tools/run_tests.rb
+++ tools/run_tests.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+
 def show_errors(name,type,errors)
   if errors.match /\S/
     puts "Differences found in #{type} test for #{name}: -reference, +ours"
@@ -13,16 +15,41 @@ end
 ours_run = 'ours.runout'
 ours_dis = 'ours.disasm'
 
+cls = '*'
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: run_tests.rb [disasm|runner] [options]"
+
+  opts.on('-c', '--class [classname]', 'Run a specific case') do |c|
+    cls = c
+  end
+
+  opts.on('-h', '--help', 'Display this message') do
+    puts opts
+    exit
+  end
+end.parse!
+
+# Usage: 'disasm' or 'runner' to run each type of test, no args to run both
+run_disasm = (ARGV[0] == 'disasm' or ARGV[0].nil?)
+run_runner = (ARGV[0] == 'runner' or ARGV[0].nil?)
+
 here_dir = "#{Dir.pwd}/#{File.dirname($0)}"
 test_dir = "#{here_dir}/../test"
 `make all` # build the reference *.disasm,*.runout from the real jvm
-Dir.glob("#{test_dir}/*.java") do |src|
+Dir.glob("#{test_dir}/#{cls}.java") do |src|
   name = src.match(/(\w+)\.java/)[1]
-  # compare disas output
-  `#{here_dir}/../console/disassembler.coffee #{test_dir}/#{name}.class >#{ours_dis}`
-  show_errors(name,'disasm',`#{here_dir}/cleandiff.sh #{test_dir}/#{name}.disasm #{ours_dis}`)
-  # compare runtime output
-  `#{here_dir}/../console/runner.coffee #{test_dir}/#{name}.class --log=error 2>&1 >#{ours_run}`
-  show_errors(name,'runtime',`diff -U0 #{test_dir}/#{name}.runout #{ours_run} | sed '1,2d'`)
+  if run_disasm
+    # compare disas output
+    `#{here_dir}/../console/disassembler.coffee #{test_dir}/#{name}.class >#{ours_dis}`
+    show_errors(name,'disasm',`#{here_dir}/cleandiff.sh #{test_dir}/#{name}.disasm #{ours_dis}`)
+  end
+  if run_runner
+    # compare runtime output
+    `#{here_dir}/../console/runner.coffee #{test_dir}/#{name}.class --log=error 2>&1 >#{ours_run}`
+    show_errors(name,'runtime',`diff -U0 #{test_dir}/#{name}.runout #{ours_run} | sed '1,2d'`)
+  end
 end
-File.unlink ours_dis,ours_run
+
+File.unlink ours_dis if File.exists? ours_dis
+File.unlink ours_run if File.exists? ours_run
