ee2e5840c8dbde120add2c454c2c7a60f9d129d7
Jez Ng <jezreel@gmail.com>
2012-03-11 09:12:19 -0400

    Add more tests for Strings.

--- test/Strings.java
+++ test/Strings.java
@@ -5,16 +5,25 @@ public class Strings {
     String a = "alphabet";
     String b = "belvedere";
     String c = "AlPhAbEt";
+    String d = "  \t alphabet  \n \t ";
     int ia = 5;
     int ib = 6;
 
     System.out.println(a.contains("phab"));
     System.out.println(a.contains(b));
+    System.out.println(a.startsWith("alpha"));
     System.out.println(a.endsWith("bet"));
     System.out.println(a.toUpperCase());
+    System.out.println(a.toLowerCase());
     System.out.println(a.equalsIgnoreCase(c));
     System.out.println(a.lastIndexOf("a"));
     System.out.println(a.length());
     System.out.println(a.substring(4,6));
+    System.out.println(a.toString());
+    System.out.println(d.trim());
+    System.out.println(a.replace('a','z'));
+    System.out.println(String.valueOf(1));
+    //System.out.println(String.valueOf(1.5));
+    //System.out.format("%s ", "asdf");
   }
 }
