e94d9bc78099188fe609ed64d0bcc1a67a481381
CJ Carey <perimosocordiae@gmail.com>
2012-03-13 12:34:10 -0400

    adding test for long ops, oh man does it fail…

---
+++ test/LongMath.java
@@ -0,0 +1,55 @@
+package test;
+
+public class LongMath {
+  public static void main(String[] args) {
+    long a = 1;
+    long b = 0;
+    long c;
+    try {
+      c = a / b;
+    }
+    catch (ArithmeticException e) {
+      System.out.println("Caught ArithmeticException as expected: " + e.getMessage());
+    }
+
+    try {
+      c = a % b;
+    }
+    catch (ArithmeticException e) {
+      System.out.println("Caught ArithmeticException as expected: " + e.getMessage());
+    }
+
+    runOps(5, 3);
+    runOps(-5, 3);
+    runOps(5, -3);
+    runOps(Long.MAX_VALUE, Long.MAX_VALUE);
+    runOps(Long.MAX_VALUE, Long.MIN_VALUE);
+    runOps(Long.MIN_VALUE, Long.MAX_VALUE);
+    runOps(Long.MIN_VALUE, Long.MIN_VALUE);
+
+    runSmallOps(5, 3);
+    runSmallOps(-5, 3);
+    runSmallOps(5, -3);
+  }
+
+  public static void runOps(long a, long b) {
+    long c = a + b;
+    System.out.println(a + " + " + b + " = " + c);
+    c = a - b;
+    System.out.println(a + " - " + b + " = " + c);
+    c = a / b;
+    System.out.println(a + " / " + b + " = " + c);
+    c = a % b;
+    System.out.println(a + " % " + b + " = " + c);
+  }
+
+  // our implementation has yet to support these operations on large integers.
+  public static void runSmallOps(long a, long b) {
+    long c = a * b;
+    System.out.println(a + " * " + b + " = " + c);
+    c = a << b;
+    System.out.println(a + " << " + b + " = " + c);
+    c = a >> b;
+    System.out.println(a + " >> " + b + " = " + c);
+  }
+}
