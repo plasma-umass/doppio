736355f1b321f9068fb38dd80df9f9c146926fa7
CJ Carey <perimosocordiae@gmail.com>
2012-03-14 17:51:53 -0400

    also ignore float and double values… because we don't want to actually emulate single precision floating point

--- tools/cleandiff.sh
+++ tools/cleandiff.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 # Remove comments, ignore whitespace, and show the diff with zero lines of
-# context
-
-diff -w -B -U0 <(sed "s/\/\/.*$//" $1) <(sed "s/\/\/.*$//" $2) | sed '1,2d'
+# context. Also ignore the specifics of how doubles and floats are printed...
+diff -w -B -U0 <(sed "s/\/\/.*$//" $1 | sed "s/float	.*/float/" | sed "s/double	.*/double/") \
+               <(sed "s/\/\/.*$//" $2 | sed "s/float	.*/float/" | sed "s/double	.*/double/") \
+               | sed '1,2d'
