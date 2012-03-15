82d46a9fd513c8f7c63d44745013ddd7f3710c82
Jez Ng <jezreel@gmail.com>
2012-03-14 22:09:31 -0400

    Test for lushr, & remove distinction between short and long tests.

--- test/LongMath.java
+++ test/LongMath.java
@@ -26,10 +26,6 @@ public class LongMath {
     runOps(Long.MAX_VALUE, Long.MIN_VALUE);
     runOps(Long.MIN_VALUE, Long.MAX_VALUE);
     runOps(Long.MIN_VALUE, Long.MIN_VALUE);
-
-    runSmallOps(5, 3);
-    runSmallOps(-5, 3);
-    runSmallOps(5, -3);
   }
 
   public static void runOps(long a, long b) {
@@ -41,16 +37,14 @@ public class LongMath {
     System.out.println(a + " / " + b + " = " + c);
     c = a % b;
     System.out.println(a + " % " + b + " = " + c);
-  }
-
-  // our implementation has yet to support these operations on large integers.
-  public static void runSmallOps(long a, long b) {
-    long c = a * b;
+    c = a * b;
     System.out.println(a + " * " + b + " = " + c);
     c = a << b;
     System.out.println(a + " << " + b + " = " + c);
     c = a >> b;
     System.out.println(a + " >> " + b + " = " + c);
+    c = a >>> b;
+    System.out.println(a + " >>> " + b + " = " + c);
     c = a | b;
     System.out.println(a + " | " + b + " = " + c);
     c = a ^ b;
