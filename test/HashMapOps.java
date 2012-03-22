74f3b2a3a1d1ac55cf8611187d4739652ad8ce3b
CJ Carey <perimosocordiae@gmail.com>
2012-03-22 14:08:01 -0400

    adding case that demonstrates empty entrySet iteration issues

--- test/HashMapOps.java
+++ test/HashMapOps.java
@@ -1,6 +1,6 @@
 // test basic hash map operations
 package test;
-import java.util.HashMap;
+import java.util.*;
 public class HashMapOps {
   public static void main(String[] args) {
     HashMap<String,Integer> foo = new HashMap<String,Integer>();
@@ -11,5 +11,13 @@ public class HashMapOps {
     int b = foo.get("world");
     System.out.println("hello is " + a);
     System.out.println("world is " + b);
+
+    foo.clear();
+    Set<Map.Entry<String, Integer>> es = foo.entrySet();
+    if (es.size() != 0)
+        throw new RuntimeException("foo should have a 0-size entrySet, had size "+es.size());
+    for (Map.Entry<String,Integer> e : es) {
+        throw new RuntimeException("Should not have gotten here!");
+    }
   }
 }
