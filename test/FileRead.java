e78845ba6684a91de1786e74e00610a48c076b4a
Jez Ng <jezreel@gmail.com>
2012-04-21 05:30:01 -0400

    Update.

--- test/FileRead.java
+++ test/FileRead.java
@@ -1,13 +1,10 @@
 // read a local file
-// this is almost identical to the copy in test/, but the
-// hardcoded filename is changed so that it works in the browser.
-
 package test;
 import java.io.*;
 public class FileRead {
   public static void main(String[] args) {
     try {
-      BufferedReader br = new BufferedReader(new FileReader("FileRead.java"));
+      BufferedReader br = new BufferedReader(new FileReader("./test/FileRead.java"));
       String line;
       while ((line = br.readLine()) != null){
         System.out.println(line);
