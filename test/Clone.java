612721c53a9ea92e7c2a2b65052a4664edebfd27
CJ Carey <perimosocordiae@gmail.com>
2012-10-07 17:36:38 -0400

    adding direct array-clone case

--- test/Clone.java
+++ test/Clone.java
@@ -1,4 +1,5 @@
 package test;
+import java.util.Arrays;
 
 public class Clone implements Cloneable {
   private static final int NDIM = 3;
@@ -22,16 +23,24 @@ public class Clone implements Cloneable {
     }
   }
   public String toString() {
-    return "["+vals[0]+","+vals[1]+","+vals[2]+"]";
+    return Arrays.toString(vals);
   }
   public static void main(String[] args) {
+    {
     Clone c = new Clone();
     Clone c2 = (Clone)c.clone();
-    System.out.println("c orig: "+c);
-    System.out.println("c2 orig: "+c2);
     c.vals[1] = 0;
     c2.vals[2] = 0;
     System.out.println("c: "+c);
     System.out.println("c2: "+c2);
+    }
+    {
+    int[] foo = {1,2,3};
+    int[] bar = foo.clone();
+    bar[2] = 0;
+    foo[1] = 0;
+    System.out.println("foo: "+Arrays.toString(foo));
+    System.out.println("bar: "+Arrays.toString(bar));
+    }
   }
 }
