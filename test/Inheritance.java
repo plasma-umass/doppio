1c398eb11edf9b91873553d99fc1d96d66947a2a
John Vilk <jvilk@cs.umass.edu>
2012-11-04 19:17:41 -0500

    Added another case to Inheritance.java -- accessing parent's field through parent's method.

--- test/Inheritance.java
+++ test/Inheritance.java
@@ -6,13 +6,16 @@ public class Inheritance {
 		child.a = 3;
 		child.b = 5;
 		System.out.println("Child a: " + child.a);
+		System.out.println("Parent a through child via getter: " + child.getA());
 		System.out.println("Child b: " + child.b);
 		AParent par = child;
 		par.a = 4;
 		System.out.println("Parent a: " + par.a);
+		System.out.println("Parent a through getter: " + par.getA());
 		System.out.println("Parent b: " + par.b);
 		BChild child2 = (BChild) par;	
 		System.out.println("Child a: " + child2.a);
+		System.out.println("Parent a through child via getter: " + child2.getA());
 		System.out.println("Child b: " + child2.b);
 	}
 }
@@ -20,6 +23,9 @@ public class Inheritance {
 class AParent {
 	public int a;
 	public int b;
+	int getA() {
+		return a;
+	}
 }
 
 class BChild extends AParent {
