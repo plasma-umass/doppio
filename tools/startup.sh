593a3f4b886c53d2b527024b0fb7803ebc735f67
Jez Ng <me@jezng.com>
2012-11-29 18:18:12 -0500

    Remove startup.sh.

--- tools/startup.sh
+++
@@ -1,9 +0,0 @@
-# Warning: Do not run this as a script! Instead, do:
-# user@host$ . startup.sh
-# This runs the commands in your current terminal session.
-
-tools/webrick.rb --dev &
-make development
-coffee -wc */*.coffee &
-tools/watch.coffee &
-open http://localhost:8000/
