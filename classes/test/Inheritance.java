fc09f86b8980e4d3d8013f45d557f0bf1c59c1bb
John Vilk <jvilk@cs.umass.edu>
2013-02-25 17:17:34 -0500

    Removing an unneeded print from the test.

--- classes/test/Inheritance.java
+++ classes/test/Inheritance.java
@@ -2,14 +2,12 @@ package classes.test;
 
 public class Inheritance {
 	public Inheritance() {
-		String blargh = "I'm Nigel Thornberry!";
 		BChild ch = null;
 		try {
 			System.out.println("a: " + ch.a);
 		} catch (NullPointerException e) {
 			System.out.println("Cannot getfield on a null object.");
 		}
-		System.out.println(blargh);
 
 		BChild child = new BChild();
 		child.a = 3;
