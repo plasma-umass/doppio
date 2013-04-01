551640d63863e6f46947e28cf0efd05267449bc8
CJ Carey <perimosocordiae@gmail.com>
2013-04-01 17:50:50 -0400

    sprucing up the test

--- classes/test/ConcurrentClinit.java
+++ classes/test/ConcurrentClinit.java
@@ -1,14 +1,11 @@
 package classes.test;
 
 class ConcurrentClinit {
-
-  static Object obj = new Object();
   static int state = 0;
 
   static class Foo {
     static {
-      System.out.println("Initializing Foo with thread " +
-          Thread.currentThread().getName());
+      System.out.println("Initializing Foo with " + Thread.currentThread().getName());
       state = 1;
       try {
         Thread.sleep(100);
@@ -16,35 +13,37 @@ class ConcurrentClinit {
       catch (InterruptedException e) {
         System.out.println("Interrupted");
       }
-      System.out.println("Finished initializing Foo with thread " +
-          Thread.currentThread().getName());
+      System.out.println("Finished initializing Foo with " + Thread.currentThread().getName());
+    }
+    Foo(String whence) {
+      System.out.println("Made a Foo from "+whence);
     }
   }
 
   static class Bar {
     static {
-      System.out.println("Initializing Bar with thread " +
-          Thread.currentThread().getName());
+      System.out.println("Initializing Bar with " + Thread.currentThread().getName());
       state = 2;
-      new Foo();
-      System.out.println("Finished initializing Bar with thread " +
-          Thread.currentThread().getName());
+      new Foo("Bar");
+      System.out.println("Finished initializing Bar with " + Thread.currentThread().getName());
     }
   }
 
   public static void main(String[] args) throws InterruptedException {
     Thread a = new Thread(new Runnable() {
       public void run() {
-        new Foo();
+        new Foo("main");
       }
     });
-    a.start();
-    while (state != 1) Thread.yield();
     Thread b = new Thread(new Runnable() {
       public void run() {
         new Bar();
       }
     });
+
+    a.start();
+    while (state != 1) Thread.yield();
+
     b.start();
     while (state != 2) Thread.yield();
     a.interrupt();
