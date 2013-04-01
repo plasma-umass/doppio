88f35452ec22e6128f023b0445c03ce484e453f6
CJ Carey <perimosocordiae@gmail.com>
2013-04-01 15:36:45 -0400

    fixing up the Synchronized test

--- classes/test/Synchronized.java
+++ classes/test/Synchronized.java
@@ -3,13 +3,14 @@ package classes.test;
 class Synchronized {
 
   static int state = 0;
+  static final Foo foo = new Foo();
 
   static class Foo {
     synchronized void bar() {
       System.out.println("Starting bar");
       state = 1;
       try {
-        Thread.sleep(100);
+        Thread.sleep(100);  // sleep doesn't give up the lock on foo
       }
       catch (InterruptedException e) {
         System.out.println("bar interrupted");
@@ -23,37 +24,28 @@ class Synchronized {
   }
 
   static class BarRunner implements Runnable {
-    Foo foo;
-    public BarRunner(Foo foo) {
-      this.foo = foo;
-    }
-
     public void run() {
       foo.bar();
     }
   }
 
   static class BazRunner implements Runnable {
-    Foo foo;
-    public BazRunner(Foo foo) {
-      this.foo = foo;
-    }
-
     public void run() {
+      System.out.println("About to run baz");
+      state = 2;
       foo.baz();
     }
   }
 
   public static void main(String[] args) {
-    Foo foo = new Foo();
-    Thread a = new Thread(new BarRunner(foo));
-    Thread b = new Thread(new BazRunner(foo));
+    Thread a = new Thread(new BarRunner());
+    Thread b = new Thread(new BazRunner());
     a.start();
     while (state != 1) Thread.yield();
     b.start();
     // if synchronization is implemented correctly, b will only run after a has
     // been interrupted, despite the yield().
-    Thread.yield();
+    while (state != 2) Thread.yield();
     a.interrupt();
   }
 
