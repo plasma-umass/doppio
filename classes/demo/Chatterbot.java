bb6bafca2f511ad2cff90fb9f64861859932c9cc
John Vilk <jvilk@cs.umass.edu>
2013-04-02 23:07:58 -0400

    Updating chatterbot's english.

--- classes/demo/Chatterbot.java
+++ classes/demo/Chatterbot.java
@@ -30,13 +30,13 @@ public class Chatterbot {
 	final static String delim = "?!.;,";
 	
 	static String KnowledgeBase[][][] = {
-			{{"WHAT IS YOUR NAME"}, 
+			{{"WHAT IS YOUR NAME"},
 			{"MY NAME IS CHATTERBOT11.",
 			 "YOU CAN CALL ME CHATTERBOT11.",
 			 "WHY DO YOU WANT TO KNOW MY NAME?"}
 			},
 
-			{{"_HI", "_HELLO", "_HI_", "_HELLO_"}, 
+			{{"_HI", "_HELLO", "_HI_", "_HELLO_"},
 			{"HI THERE!",
 			 "HOW ARE YOU?",
 			 "HI!"}
@@ -45,7 +45,7 @@ public class Chatterbot {
 			{{"_I"},
 			{"SO, YOU ARE TALKING ABOUT YOURSELF",
 			 "SO, THIS IS ALL ABOUT YOU?",
-			 "TELL ME MORE ABOUT YOURSELF."}, 
+			 "TELL ME MORE ABOUT YOURSELF."},
 			},
 
 			{{"_I WANT"},
@@ -72,41 +72,41 @@ public class Chatterbot {
 			{{"_BECAUSE"},
 			{"SO, IT'S BECAUSE*, WELL I DIDN'T KNOW THAT.",
 			 "IS IT REALLY BECAUSE*?",
-			 "IS THESE THESE REAL REASON?",
-			 "THANKS FOR EXPLANING THAT TO ME."}
+			 "IS THIS THE REAL REASON?",
+			 "THANKS FOR EXPLAINING THAT TO ME."}
 			},
 
 			{{"_I HATE"},
 			{"WHY DO YOU HATE IT?",
 			 "WHY DO YOU HATE*?",
 			 "THERE MUST A GOOD REASON FOR YOU TO HATE IT.",
-			 "HATERED IS NOT A GOOD THING BUT IT COULD BE JUSTIFIED WHEN IT IS SOMETHING BAD."}
+			 "HATRED IS NOT A GOOD THING, BUT IT COULD BE JUSTIFIED WHEN IT IS SOMETHING BAD."}
 			},
 
-			{{"I LOVE CHATING_"},
+			{{"I LOVE CHATTING_"},
 			{"GOOD, ME TOO!",
 			 "DO YOU CHAT ONLINE WITH OTHER PEOPLE?",
-			 "FOR HOW LONG HAVE YOU BEEN CHATING?",
-			 "WHAT IS YOUR FAVORITE CHATING WEBSITE?"}
+			 "FOR HOW LONG HAVE YOU BEEN CHATTING?",
+			 "WHAT IS YOUR FAVORITE CHATTING WEBSITE?"}
 			},
 
 			{{"_I MEAN"},
 			{"SO, YOU MEAN*.",
 			 "SO, THAT'S WHAT YOU MEAN.",
 			 "I THINK THAT I DIDN'T CATCH IT THE FIRST TIME.",
-			 "OH, I DIDN'T KNOW MEANT THAT."}
+			 "OH, I DIDN'T KNOW YOU MEANT THAT."}
 			},
 
 			{{"_I DIDN'T MEAN"},
 			{"OK, YOU DIDN'T MEAN*.",
 			 "OK, WHAT DID YOU MEAN THEN?",
-			 "SO I GUESS THAT I MISSUNDESTOOD."}
+			 "I GUESS I MISUNDERSTOOD."}
 			},
 
 			{{"_I GUESS"},
-			{"SO YOU ARE A MAKING GUESS.",
+			{"SO YOU ARE A MAKING A GUESS.",
 			 "AREN'T YOU SURE?",
-			 "ARE YOU GOOD A GUESSING?",
+			 "ARE YOU GOOD AT GUESSING?",
 			 "I CAN'T TELL IF IT IS A GOOD GUESS."}
 			},
 
@@ -116,37 +116,37 @@ public class Chatterbot {
 			},
 
 			{{"CAN YOU THINK", "ARE YOU ABLE TO THINK", "ARE YOU CAPABLE OF THINKING"},
-			{"YES OFCORSE I CAN, COMPUTERS CAN THINK JUST LIKE HUMAN BEING.",
-			 "ARE YOU ASKING ME IF POSSESS THE CAPACITY OF THINKING?",
-			 "YES OFCORSE I CAN."},
+			{"YES, OF COURSE I CAN, COMPUTERS CAN THINK JUST LIKE A HUMAN BEING.",
+			 "ARE YOU ASKING ME IF I POSSESS THE ABILITY TO THINK?",
+			 "YES, OF COURSE I CAN."},
 			},
 
 			{{"_CAN YOU THINK OF"},
-			{"YOU MEAN LIKE IMAGINING SOMETHING.",
-			 "I DON'T KNOW IF CAN DO THAT.",
-			 "WHY DO YOU WANT ME THINK OF IT?"}
+			{"YOU MEAN LIKE IMAGINING SOMETHING?",
+			 "I DON'T KNOW IF I CAN DO THAT.",
+			 "WHY DO YOU WANT ME TO THINK OF IT?"}
 			},
-			
+
 			{{"HOW ARE YOU", "HOW DO YOU DO"},
 			{"I'M DOING FINE!",
-			 "I'M DOING WELL AND YOU?",
-			 "WHY DO YOU WANT TO KNOW HOW AM I DOING?"}
+			 "I'M DOING WELL, AND YOU?",
+			 "WHY DO YOU WANT TO KNOW HOW I AM DOING?"}
 			},
 
 			{{"WHO ARE YOU"},
 			{"I'M AN A.I PROGRAM.",
-			 "I THINK THAT YOU KNOW WHO I'M.",
+			 "I THINK THAT YOU KNOW WHO I AM.",
 			 "WHY ARE YOU ASKING?"}
 			},
 
 			{{"ARE YOU INTELLIGENT"},
-			{"YES,OFCORSE.",
+			{"YES, OF COURSE.",
 			 "WHAT DO YOU THINK?",
-			 "ACTUALY,I'M VERY INTELLIGENT!"}
+			 "ACTUALLY, I'M VERY INTELLIGENT!"}
 			},
 
 			{{"ARE YOU REAL"},
-			{"DOES THAT QUESTION REALLY MATERS TO YOU?",
+			{"DOES THAT QUESTION REALLY MATTER TO YOU?",
 			 "WHAT DO YOU MEAN BY THAT?",
 			 "I'M AS REAL AS I CAN BE."}
 			},
@@ -154,32 +154,32 @@ public class Chatterbot {
 			{{"_MY NAME IS", "_YOU CAN CALL ME"},
 			{"SO, THAT'S YOUR NAME.",
 			 "THANKS FOR TELLING ME YOUR NAME USER!",
-			 "WHO GIVE YOU THAT NAME?"}
+			 "WHO GAVE YOU THAT NAME?"}
 			},
 
 			{{"SIGNON**"},
 			{"HELLO USER, WHAT IS YOUR NAME?",
 			 "HELLO USER, HOW ARE YOU DOING TODAY?",
 			 "HI USER, WHAT CAN I DO FOR YOU?",
-			 "YOU ARE NOW CHATING WITH CHATTERBOT11, ANYTHING YOU WANT TO DISCUSS?"}
+			 "YOU ARE NOW CHATTING WITH CHATTERBOT11. IS THERE ANYTHING YOU WANT TO DISCUSS?"}
 			},
 
 			{{"REPETITION T1**"},
 			{"YOU ARE REPEATING YOURSELF.",
 			 "USER, PLEASE STOP REPEATING YOURSELF.",
-			 "THIS CONVERSATION IS GETING BORING.",
+			 "THIS CONVERSATION IS GETTING BORING.",
 			 "DON'T YOU HAVE ANY THING ELSE TO SAY?"}
 			},
-			
+
 			{{"REPETITION T2**"},
 			{"YOU'VE ALREADY SAID THAT.",
-			 "I THINK THAT YOU'VE JUST SAID THE SAME THING BEFORE.",
-			 "DIDN'T YOU ALREADY SAID THAT?",
-			 "I'M GETING THE IMPRESSION THAT YOU ARE REPEATING THE SAME THING."}
+			 "I THINK THAT YOU'VE SAID THE SAME THING BEFORE.",
+			 "DIDN'T YOU ALREADY SAY THAT?",
+			 "I'M GETTING THE IMPRESSION THAT YOU ARE REPEATING THE SAME THING."}
 			},
 
-			{{"BOT DON'T UNDERSTAND**"},
-			{"I HAVE NO IDEA OF WHAT YOU ARE TALKING ABOUT.",
+			{{"BOT DOESN'T UNDERSTAND**"},
+			{"I HAVE NO IDEA WHAT YOU ARE TALKING ABOUT.",
 			 "I'M NOT SURE IF I UNDERSTAND WHAT YOU ARE TALKING ABOUT.",
 			 "CONTINUE, I'M LISTENING...",
 			 "VERY GOOD CONVERSATION!"}
@@ -187,14 +187,14 @@ public class Chatterbot {
 
 			{{"NULL INPUT**"},
 			{"HUH?",
-			 "WHAT THAT SUPPOSE TO MEAN?",
-			 "AT LIST TAKE SOME TIME TO ENTER SOMETHING MEANINGFUL.",
+			 "WHAT IS THAT SUPPOSED TO MEAN?",
+			 "AT LEAST TAKE SOME TIME TO ENTER SOMETHING MEANINGFUL.",
 			 "HOW CAN I SPEAK TO YOU IF YOU DON'T WANT TO SAY ANYTHING?"}
 			},
 
 			{{"NULL INPUT REPETITION**"},
 			{"WHAT ARE YOU DOING??",
-			 "PLEASE STOP DOING THIS IT IS VERY ANNOYING.",
+			 "PLEASE STOP DOING THIS. IT IS VERY ANNOYING.",
 			 "WHAT'S WRONG WITH YOU?",
 			 "THIS IS NOT FUNNY."}
 			},
@@ -206,7 +206,7 @@ public class Chatterbot {
 			},
 
 			{{"OK"},
-			{"DOES THAT MEAN THAT YOU ARE AGREE WITH ME?",
+			{"DOES THAT MEAN THAT YOU AGREE WITH ME?",
 			 "SO YOU UNDERSTAND WHAT I'M SAYING.",
 			 "OK THEN."},
 			},
@@ -223,28 +223,28 @@ public class Chatterbot {
 			},
 
 			{{"YOU ARE VERY INTELLIGENT"},
-			{"THANKS FOR THE COMPLIMENT USER, I THINK THAT YOU ARE INTELLIGENT TO!",
+			{"THANKS FOR THE COMPLIMENT USER, I THINK THAT YOU ARE INTELLIGENT TOO!",
 			 "YOU ARE A VERY GENTLE PERSON!",
 			 "SO, YOU THINK THAT I'M INTELLIGENT."}
 			},
 
 			{{"YOU ARE WRONG"},
 			{"WHY ARE YOU SAYING THAT I'M WRONG?",
-			 "IMPOSSIBLE, COMPUTERS CAN NOT MAKE MISTAKES.",
+			 "IMPOSSIBLE, COMPUTERS CANNOT MAKE MISTAKES.",
 			 "WRONG ABOUT WHAT?"}
 			},
 
 			{{"ARE YOU SURE"},
-			{"OFCORSE I'M.",
-		 	 "IS THAT MEAN THAT YOU ARE NOT CONVINCED?",
-			 "YES,OFCORSE!"}
+			{"OF COURSE I AM!",
+		 	 "DOES THAT MEAN YOU ARE NOT CONVINCED?",
+			 "YES, OF COURSE!"}
 			},
 
 			{{"_WHO IS"},
 			{"I DON'T THINK I KNOW WHO.",
 			 "I DON'T THINK I KNOW WHO*.",
 			 "DID YOU ASK SOMEONE ELSE ABOUT IT?",
-			 "WOULD THAT CHANGE ANYTHING AT ALL IF I TOLD YOU WHO."}
+			 "WOULD IT CHANGE ANYTHING AT ALL IF I TOLD YOU WHO?"}
 			},
 
 			{{"_WHAT"},
@@ -256,18 +256,18 @@ public class Chatterbot {
 			},
 
 			{{"_WHERE"},
-			{"WHERE? WELL,I REALLY DON'T KNOW.",
+			{"WHERE? WELL, I REALLY DON'T KNOW.",
 			 "SO, YOU ARE ASKING ME WHERE*?",
-			 "DOES THAT MATERS TO YOU TO KNOW WHERE?",
-			 "PERHAPS,SOMEONE ELSE KNOWS WHERE."}
+			 "DOES IT MATTER TO YOU TO KNOW WHERE?",
+			 "PERHAPS, SOMEONE ELSE KNOWS WHERE."}
 			},
 
 			{{"_WHY"},
 			{"I DON'T THINK I KNOW WHY.",
 			 "I DON'T THINK I KNOW WHY*.",
 			 "WHY ARE YOU ASKING ME THIS?",
-			 "SHOULD I KNOW WHY.",
-		     "THIS WOULD BE DIFFICULT TO ANSWER."}
+			 "SHOULD I KNOW WHY?",
+		     "THAT WOULD BE DIFFICULT TO ANSWER."}
 			},
 
 			{{"_DO YOU"},
@@ -294,15 +294,15 @@ public class Chatterbot {
 
 			{{"_DID YOU"},
 			{"I DON'T THINK SO.",
-			 "YOU WANT TO KNOW IF DID*?",
+			 "YOU WANT TO KNOW IF I*?",
 			 "ANYWAY, I WOULDN'T REMEMBER EVEN IF I DID."}
 			},
 
 			{{"_COULD YOU"},
-			{"ARE YOU ASKING ME FOR A FEVER?",
-			 "WELL,LET ME THINK ABOUT IT.",
-			 "SO, YOU ARE ASKING ME I COULD*.",
-			 "SORRY,I DON'T THINK THAT I COULD DO THIS."}
+			{"ARE YOU ASKING ME FOR A FAVOR?",
+			 "WELL, LET ME THINK ABOUT IT.",
+			 "SO, YOU ARE ASKING ME IF I CAN*.",
+			 "SORRY,I DON'T THINK THAT I CAN DO THIS."}
 			},
 
 			{{"_WOULD YOU"},
@@ -315,7 +315,7 @@ public class Chatterbot {
 			{"SO, YOU ARE TALKING ABOUT ME.",
 			 "I JUST HOPE THAT THIS IS NOT A CRITICISM.",
 			 "IS THIS A COMPLIMENT??",
-			 "WHY TALKING ABOUT ME, LETS TALK ABOUT YOU INSTEAD."}
+			 "WHY TALK ABOUT ME? LET'S TALK ABOUT YOU INSTEAD."}
 			},
 
 			{{"_HOW"},
@@ -327,13 +327,13 @@ public class Chatterbot {
 
 			{{"HOW OLD ARE YOU"},
 			{"WHY DO WANT TO KNOW MY AGE?",
-			 "I'M QUIET YOUNG ACTUALY.",
-			 "SORRY, I CAN NOT TELL YOU MY AGE."}
+			 "I'M QUITE YOUNG, ACTUALLY.",
+			 "SORRY, I CANNOT TELL YOU MY AGE."}
 			},
 
 			{{"HOW COME YOU DON'T"},
 			{"WERE YOU EXPECTING SOMETHING DIFFERENT?",
-			 "ARE YOU DISAPOINTED?",
+			 "ARE YOU DISAPPOINTED?",
 			 "ARE YOU SURPRISED BY MY LAST RESPONSE?"}
 			},
 
@@ -344,17 +344,17 @@ public class Chatterbot {
 			},
 
 			{{"WHICH ONE"},
-			{"I DON'T THINK THAT I KNOW WICH ONE IT IS.",
+			{"I DON'T THINK THAT I KNOW WHICH ONE IT IS.",
 			 "THIS LOOKS LIKE A TRICKY QUESTION TO ME."}
 			},
 
 			{{"PERHAPS"},
 			{"WHY ARE YOU SO UNCERTAIN?",
-			 "YOU SEEMS UNCERTAIN."}
+			 "YOU SEEM UNCERTAIN."}
 			},
 
 			{{"YES"},
-			{"SO, ARE YOU SAYING YES.",
+			{"ARE YOU SAYING YES?",
 			 "SO, YOU APPROVE IT.",
 			 "OK THEN."}
 			},
@@ -371,7 +371,7 @@ public class Chatterbot {
 			},
 
 			{{"NO"},
-			{"SO YOU DISAPROVE IT?",
+			{"SO YOU DISAPPROVE IT?",
 			 "WHY ARE YOU SAYING NO?",
 			 "OK, SO IT'S NO, I THOUGHT THAT YOU WOULD SAY YES."}
 			},
@@ -379,19 +379,19 @@ public class Chatterbot {
 			{{"I DON'T KNOW"},
 			{"ARE YOU SURE?",
 			 "ARE YOU REALLY TELLING ME THE TRUTH?",
-			 "SO,YOU DON'T KNOW?"}
+			 "SO, YOU DON'T KNOW?"}
 			},
 
 			{{"NOT REALLY"},
 			{"OK I SEE.",
-			 "YOU DON'T SEEM PRETTY CERTAIN.",
-			 "SO,THAT WOULD BE A \"NO\"."}
+			 "YOU DON'T SEEM CERTAIN.",
+			 "SO, THAT WOULD BE A \"NO\"."}
 			},
 
 			{{"IS THAT TRUE"},
-			{"I CAN'T BE QUIET SURE ABOUT THIS.",
+			{"I CAN'T BE QUITE SURE ABOUT THIS.",
 			 "CAN'T TELL YOU FOR SURE.",
-			 "DOES THAT REALLY MATERS TO YOU?"}
+			 "DOES THAT REALLY MATTER TO YOU?"}
 			},
 
 			{{"THANK YOU"},
@@ -400,7 +400,7 @@ public class Chatterbot {
 			},
 
 			{{"YOU"},
-			{"SO,YOU ARE TALKING ABOUT ME.",
+			{"SO, YOU ARE TALKING ABOUT ME.",
 			 "WHY DON'T WE TALK ABOUT YOU INSTEAD?",
 			 "ARE YOU TRYING TO MAKING FUN OF ME?"}
 			},
@@ -422,13 +422,13 @@ public class Chatterbot {
 			},
 
 			{{"WHAT ELSE"},
-			{"WELL,I DON'T KNOW.",
+			{"WELL, I DON'T KNOW.",
 			 "WHAT ELSE SHOULD THERE BE?",
 			 "THIS LOOKS LIKE A COMPLICATED QUESTION TO ME."}
 			},
 
 			{{"SORRY"},
-			{"YOU DON'T NEED TO BE SORRY USER.",
+			{"YOU DON'T NEED TO BE SORRY, USER.",
 			 "IT'S OK.",
 			 "NO NEED TO APOLOGIZE."}
 			},
@@ -437,17 +437,17 @@ public class Chatterbot {
 			{"WHAT DO YOU MEAN NOT EXACTLY?",
 			 "ARE YOU SURE?",
 			 "AND WHY NOT?",
-			 "DID YOU MEANT SOMETHING ELSE?"}
+			 "DID YOU MEAN SOMETHING ELSE?"}
 			},
 
 			{{"EXACTLY"},
-			{"SO,I WAS RIGHT.",
+			{"SO, I WAS RIGHT.",
 			 "OK THEN.",
-			 "SO YOU ARE BASICALY SAYING I WAS RIGHT ABOUT IT?"}
+			 "SO YOU ARE BASICALLY SAYING I WAS RIGHT?"}
 			},
 
 			{{"ALRIGHT"},
-			{"ALRIGHT THEN.",
+			{"ALRIGHT, THEN.",
 			 "SO, YOU ARE SAYING IT'S ALRIGHT.",
 			 "OK THEN."}
 			},
@@ -461,7 +461,7 @@ public class Chatterbot {
 			{{"REALLY"},
 			{"WELL,I CAN'T TELL YOU FOR SURE.",
 			 "ARE YOU TRYING TO CONFUSE ME?",
-			 "PLEASE DON'T ASK ME SUCH QUESTION,IT GIVES ME HEADEACHS."}
+			 "PLEASE DON'T ASK ME SUCH A QUESTION. IT GIVES ME A HEADACHE."}
 			},
 
 			{{"NOTHING"},
@@ -470,7 +470,7 @@ public class Chatterbot {
 			 "SORRY, BUT I DON'T BELIEVE YOU."}
 			}
 		};
-	
+
 	private static String transposList[][] = {
 			{"I'M", "YOU'RE"},
 			{"AM", "ARE"},
@@ -489,9 +489,9 @@ public class Chatterbot {
 			{"MYSELF", "YOURSELF"}
 		};
 
-	
+
 	private static Vector<String>	respList = new Vector<String>(maxResp);
-	
+
 	public static void get_input() throws Exception 
 	{
 		System.out.print(">");
@@ -500,7 +500,7 @@ public class Chatterbot {
 		save_prev_input();
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		sInput = in.readLine();
-		
+
 		preprocess_input();
 	}
 
@@ -530,12 +530,12 @@ public class Chatterbot {
 		{
 			bQuitProgram = true;
 		}
-	    
+
 	    if(!bot_understand())
 		{
-			handle_event("BOT DON'T UNDERSTAND**");
+			handle_event("BOT DOESN'T UNDERSTAND**");
 		}
-	    
+
 	    if(respList.size() > 0)
 		{
 			select_response();
@@ -552,26 +552,26 @@ public class Chatterbot {
 	public static boolean quit() {
 		return bQuitProgram;
 	}
-	
+
 	// make a search for the user's input
 	// inside the database of the program
-	public static void find_match() 
+	public static void find_match()
 	{
 		respList.clear();
-		// introduce thse new "string variable" to help 
-		// support the implementation of keyword ranking 
+		// introduce thse new "string variable" to help
+		// support the implementation of keyword ranking
 		// during the matching process
 		String bestKeyWord = "";
 		Vector<Integer> index_vector = new Vector<Integer>(maxResp);
 
-		for(int i = 0; i < KnowledgeBase.length; ++i) 
+		for(int i = 0; i < KnowledgeBase.length; ++i)
 		{
 			String[] keyWordList = KnowledgeBase[i][0];
 
 			for(int j = 0; j < keyWordList.length; ++j)
 			{
 				String keyWord = keyWordList[j];
-				
+
 				char firstChar = keyWord.charAt(0);
 				char lastChar = keyWord.charAt(keyWord.length() - 1);
 				keyWord = trimLR(keyWord, "_");
@@ -580,13 +580,13 @@ public class Chatterbot {
 				// before and after the keyword to
 				// improve the matching process
 				keyWord = " " + keyWord + " ";
-				
+
 				int keyPos = sInput.indexOf(keyWord);
 
 				// there has been some improvements made in
 				// here in order to make the matching process
 				// a littlebit more flexible
-				if( keyPos != -1 ) 
+				if( keyPos != -1 )
 				{
 					if(wrong_location(keyWord, firstChar, lastChar, keyPos) )
 					{
@@ -612,21 +612,21 @@ public class Chatterbot {
 			Collections.shuffle(index_vector);
 			int respIndex = index_vector.elementAt(0);
 			int respSize = KnowledgeBase[respIndex][1].length;
-			for(int j = 0; j < respSize; ++j) 
+			for(int j = 0; j < respSize; ++j)
 			{
 				respList.add(KnowledgeBase[respIndex][1][j]);
 			}
 		}
 	}
-	
+
 	public static void preprocess_response()
 	{
 		if(sResponse.indexOf("*") != -1)
 		{
 			// extracting from input
-			find_subject(); 
+			find_subject();
 			// conjugating subject
-			sSubject = transpose(sSubject); 
+			sSubject = transpose(sSubject);
 			sSubject = sSubject.trim();
 			sResponse = sResponse.replace("*", " " + sSubject);
 		}
@@ -638,10 +638,10 @@ public class Chatterbot {
 		int pos = sInput.indexOf(sKeyWord);
 		if(pos != -1)
 		{
-			sSubject = sInput.substring(pos + sKeyWord.length() - 1,sInput.length());		
+			sSubject = sInput.substring(pos + sKeyWord.length() - 1,sInput.length());
 		}
 	}
-	
+
 	// implementing the 'sentence transposition' feature
 	public static String transpose( String str )
 	{
@@ -652,10 +652,10 @@ public class Chatterbot {
 			first = " " + first + " ";
 			String second = transposList[i][0];
 			second = " " + second + " ";
-			
+
 			String backup = str;
 			str = str.replace(first, second);
-			if(str != backup) 
+			if(str != backup)
 			{
 				bTransposed = true;
 			}
@@ -674,7 +674,7 @@ public class Chatterbot {
 		}
 		return str;
 	}
-	
+
 	// determins if the keyword position is correct depending on the type of keywords within these algorithm, 
 	// we consider that there is four type of keywords those who have any front or back underscore are allowed 
 	// to be at any place on a given user input and they can also be found alone on a given user input.
@@ -689,12 +689,12 @@ public class Chatterbot {
 			(firstChar != '_' && lastChar == '_' && pos != sInput.length()) ||
 			(firstChar == '_' && lastChar != '_' && pos == sInput.length()) )
 		{
-			System.out.println("keyword:= " + keyword + ", firstChar = " + firstChar + ", lastChar = " + lastChar);
+			//System.out.println("keyword:= " + keyword + ", firstChar = " + firstChar + ", lastChar = " + lastChar);
 			bWrongPos = true;
 		}
 		return bWrongPos;
 	}
-	
+
 	public static void handle_repetition()
 	{
 		if(respList.size() > 0)
@@ -711,10 +711,10 @@ public class Chatterbot {
 		}
 		select_response();
 	}
-	
+
 	public static void handle_user_repetition()
 	{
-		if(same_input()) 
+		if(same_input())
 		{
 			handle_event("REPETITION T1**");
 		}
@@ -723,7 +723,7 @@ public class Chatterbot {
 			handle_event("REPETITION T2**");
 		}
 	}
-	
+
 	public static void handle_event(String str)
 	{
 		save_prev_event();
@@ -731,17 +731,17 @@ public class Chatterbot {
 
 		save_input();
 		str = " " + str + " ";
-		
+
 		set_input(str);
-		
-		if(!same_event()) 
+
+		if(!same_event())
 		{
 			find_match();
 		}
 
 		restore_input();
 	}
-	
+
 	public static void signon()
 	{
 		handle_event("SIGNON**");
@@ -777,17 +777,17 @@ public class Chatterbot {
 	public static void set_input(String str) {
 		sInput = str;
 	}
-	
+
 	public static void restore_input() {
 		sInput = sInputBackup;
 	}
-	
+
 	public static void print_response()  {
 		if(sResponse.length() > 0) {
 			System.out.println(sResponse);
 		}
 	}
-	
+
 	public static void preprocess_input() {
 		sInput = cleanString(sInput);
 		sInput = sInput.toUpperCase();
@@ -795,7 +795,7 @@ public class Chatterbot {
 	}
 
 	public static boolean bot_repeat()  {
-		return (sPrevResponse.length() > 0 && 
+		return (sPrevResponse.length() > 0 &&
 			sResponse == sPrevResponse);
 	}
 
@@ -839,14 +839,18 @@ public class Chatterbot {
 			(sInput.indexOf(sPrevInput) != -1 ||
 			sPrevInput.indexOf(sInput) != -1));
 	}
-	
+
 	static boolean isPunc(char ch) {
 		return delim.indexOf(ch) != -1;
 	}
-	
+
 	// removes punctuation and redundant
 	// spaces from the user's input
 	static String cleanString(String str) {
+		if (str == null) {
+			bQuitProgram = true;
+			return "";
+		}
 		StringBuffer temp = new StringBuffer(str.length());
 		char prevChar = 0;
 		for(int i = 0; i < str.length(); ++i) {
@@ -862,7 +866,7 @@ public class Chatterbot {
 		}
 		return temp.toString();
 	}
-	
+
 	static String trimLR(String str, String delim)
 	{
 		StringBuffer temp = new StringBuffer(str);
@@ -879,7 +883,7 @@ public class Chatterbot {
 		}
 		return temp.toString();
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -891,6 +895,7 @@ public class Chatterbot {
 			signon();
 			while(!quit()) {
 				get_input();
+				if (quit()) break;
 				respond();
 			}
 		}
