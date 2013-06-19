0ac8fba2f15405be24a7bdbbabdde14ffb08ea3b
CJ Carey <perimosocordiae@gmail.com>
2013-06-19 03:57:06 -0400

    Removing explicit modules, converting exceptions.

--- src/logging.ts
+++ src/logging.ts
@@ -1,58 +1,59 @@
 declare module 'vendor/gLong.js' {}
 import gLong = module('vendor/gLong.js');
-module logging {
 
-  function debug_var(e: any): String {
-    if (e === null) {
-      return '!';
-    }
-    if (e === void 0) {
-      return 'undef';
-    }
-    if (e.ref != null) {
-      return "*" + e.ref;
-    }
-    if (e instanceof gLong) {
-      return "" + e + "L";
-    }
-    return e;
+// default module: logging
+
+function debug_var(e: any): String {
+  if (e === null) {
+    return '!';
   }
-
-  export function debug_vars(arr: Array): String[] {
-    return arr.map(debug_var);
+  if (e === void 0) {
+    return 'undef';
   }
-
-  // TODO: turn this into an enum, if possible
-  export var VTRACE = 10;
-  export var TRACE = 9;
-  export var DEBUG = 5;
-  export var ERROR = 1;
-  export var log_level = ERROR;
-
-  function log(level: number, msgs: any[]): void {
-    if (level <= log_level) {
-      var msg = msgs.join(' ');
-      if (level == 1) {
-        console.error(msg);
-      } else {
-        console.log(msg);
-      }
-    }
-  };
-
-  export function vtrace(...msgs: any[]): void {
-    log(VTRACE, msgs);
+  if (e.ref != null) {
+    return "*" + e.ref;
   }
-
-  export function trace(...msgs: any[]): void {
-    log(TRACE, msgs);
+  if (e instanceof gLong) {
+    return "" + e + "L";
   }
-
-  export function debug(...msgs: any[]): void {
-    log(DEBUG, msgs);
-  }
-
-  export function error(...msgs: any[]): void {
-    log(ERROR, msgs);
-  }
+  return e;
 }
+
+export function debug_vars(arr: Array): String[] {
+  return arr.map(debug_var);
+}
+
+// TODO: turn this into an enum, if possible
+export var VTRACE = 10;
+export var TRACE = 9;
+export var DEBUG = 5;
+export var ERROR = 1;
+export var log_level = ERROR;
+
+function log(level: number, msgs: any[]): void {
+  if (level <= log_level) {
+    var msg = msgs.join(' ');
+    if (level == 1) {
+      console.error(msg);
+    } else {
+      console.log(msg);
+    }
+  }
+};
+
+export function vtrace(...msgs: any[]): void {
+  log(VTRACE, msgs);
+}
+
+export function trace(...msgs: any[]): void {
+  log(TRACE, msgs);
+}
+
+export function debug(...msgs: any[]): void {
+  log(DEBUG, msgs);
+}
+
+export function error(...msgs: any[]): void {
+  log(ERROR, msgs);
+}
+
