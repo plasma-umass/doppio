4ee9e192379ad5b8de0b6f4ff62f3d1e9f8d6341
Lawrence Angrave <angrave@illinois.eduuutoomanyu>
2013-08-07 21:34:06 -0500

    Minor version bump JDK6 download links in setup.sh (previous links were 404)

--- tools/setup.sh
+++ tools/setup.sh
@@ -22,10 +22,11 @@ if [ ! -f classes/java/lang/Object.class ]; then
   DOWNLOAD_DIR=`mktemp -d jdk-download.XXX`
   cd $DOWNLOAD_DIR
     DEBS_DOMAIN="http://security.ubuntu.com/ubuntu/pool/main/o/openjdk-6"
-    DEBS=("openjdk-6-jre-headless_6b27-1.12.5-0ubuntu0.12.04.1_i386.deb"
-          "openjdk-6-jdk_6b27-1.12.5-0ubuntu0.12.04.1_i386.deb"
-          "openjdk-6-jre-lib_6b27-1.12.5-0ubuntu0.12.04.1_all.deb")
+    DEBS=("openjdk-6-jre-headless_6b27-1.12.6-1ubuntu0.12.04.2_i386.deb"
+          "openjdk-6-jdk_6b27-1.12.6-1ubuntu0.12.04.2_i386.deb"
+          "openjdk-6-jre-lib_6b27-1.12.6-1ubuntu0.12.04.2_all.deb")
     for DEB in ${DEBS[@]}; do
+      echo "Requesting $DEB"
       wget $DEBS_DOMAIN/$DEB
       ar p $DEB data.tar.gz | tar zx
     done
