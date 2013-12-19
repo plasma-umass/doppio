e3759979866f47f78ccbf49159f44e664f89dc4f
John Vilk <jvilk@cs.umass.edu>
2013-12-19 16:11:17 -0500

    Making parts of setup.js more portable, removing bootstrapping functionality.
    In the future, we will operate under the assumption that the user runs `npm install`, since `setup.js` will likely run as a part of the Grunt process, and Grunt will require `npm install` to run before it'll function.

--- tools/setup.js
+++ tools/setup.js
@@ -2,13 +2,33 @@
 "use strict";
 var fs = require('fs'),
     path = require('path'),
+    os = require('os'),
+    zlib = require('zlib'),
     exec = require('child_process').exec,
-    async, npm;
+    ar = require('ar'),
+    tar = require('tar'),
+    request = require('request'),
+    semver = require('semver'),
+    rimraf = require('rimraf'),
+    async = require('async');
 
 var DOWNLOAD_DIR,
     JAVA = 'java',
     JAVAC = 'javac',
-    JAVAP = 'javap';
+    JAVAP = 'javap',
+    // Ubuntu (security) repo actual on 24.02.2013
+    DEBS_DOMAIN="http://security.ubuntu.com/ubuntu/pool/main/o/openjdk-6",
+    DEBS = [
+        "openjdk-6-jre-headless_6b27-1.12.6-1ubuntu0.12.04.4_i386.deb",
+        "openjdk-6-jdk_6b27-1.12.6-1ubuntu0.12.04.4_i386.deb",
+        "openjdk-6-jre-lib_6b27-1.12.6-1ubuntu0.12.04.4_all.deb"
+    ],
+    DOWNLOAD_DIR = path.resolve(os.tmpdir(), "jdk-download" + Math.floor(Math.random()*100000)),
+    RUN_DIR = path.resolve(__dirname, '..'),
+    VENDOR_DIR = path.resolve(RUN_DIR, 'vendor'),
+    CLASSES_DIR = path.resolve(VENDOR_DIR, 'classes');
+
+console.log(DOWNLOAD_DIR);
 
 /**
  * Checks if the given module is available. If not, it installs it with npm.
@@ -52,14 +72,14 @@ function find_java_home(cb) {
         key:  '\\SOFTWARE\\JavaSoft\\Java Runtime Environment\\1.6'
       });
       regKey.values(function(err, items) {
-        var i, java_home;
+        var i, java_bin;
         if (!err) {
           for (i in items) {
             if (items[i].name === 'JavaHome') {
-              java_home = items[i].value;
-              JAVA = path.resolve(java_home, 'bin\\java.exe');
-              JAVAC = path.resolve(java_home, 'bin\\javac.exe');
-              JAVAC = path.resolve(java_home, 'bin\\javap.exe');
+              java_bin = path.resolve(items[i].value, 'bin');
+              JAVA = path.resolve(java_bin, 'java.exe');
+              JAVAC = path.resolve(java_bin, 'javac.exe');
+              JAVAC = path.resolve(java_bin, 'javap.exe');
               return cb();
             }
           }
@@ -73,14 +93,14 @@ function find_java_home(cb) {
     // *nix / Mac
     // Option 1: Can we invoke 'java' directly?
     exec(JAVA, function(err, stdout, stderr) {
-      var java_home;
+      var java_bin;
       if (err) {
         // Option 2: Is JAVA_HOME defined?
         if (process.env.JAVA_HOME) {
-          java_home = process.env.JAVA_HOME;
-          JAVA = path.resolve(java_home, 'bin/java');
-          JAVAC = path.resolve(java_home, 'bin/javac');
-          JAVAC = path.resolve(java_home, 'bin/javap');
+          java_bin = path.resolve(process.env.JAVA_HOME, 'bin');
+          JAVA = path.resolve(java_bin, 'java');
+          JAVAC = path.resolve(java_bin, 'javac');
+          JAVAC = path.resolve(java_bin, 'javap');
         } else {
           // Java can't be found.
           cb(new Error(fail_msg));
@@ -93,46 +113,74 @@ function find_java_home(cb) {
   }
 }
 
-function jcl_download(cb) {
-  console.log('Downloading the Java class library (big download, may take a while)');
-  process.chdir('vendor');
-  // Ubuntu (security) repo actual on 24.02.2013
-  exec('mktemp -d jdk-download.XXX', function(err, stdout, stderr){
-    DOWNLOAD_DIR = stdout.trim();
-    process.chdir(DOWNLOAD_DIR);
-    var DEBS_DOMAIN="http://security.ubuntu.com/ubuntu/pool/main/o/openjdk-6";
-    var DEBS = [
-        "openjdk-6-jre-headless_6b27-1.12.6-1ubuntu0.12.04.4_i386.deb",
-        "openjdk-6-jdk_6b27-1.12.6-1ubuntu0.12.04.4_i386.deb",
-        "openjdk-6-jre-lib_6b27-1.12.6-1ubuntu0.12.04.4_all.deb"
-    ];
-    var request = require('request');
-    async.each(DEBS, function(deb, cb2){
-      var url = DEBS_DOMAIN + '/' + deb;
-      var file = fs.createWriteStream(deb);
-      request(url).pipe(file);
-      file.on('finish', function(){
-        file.close();
-        exec('ar p '+deb+' data.tar.gz | tar zx', function(err, stdout, stderr){
-          return cb2(err);
-        });
-      });
-    }, function(err){
-      process.chdir('..');
-      return cb(err);
+/**
+ * Finds data.tar.gz in the given debian package, and extracts it. Calls the
+ * callback with an optional error message when finished.
+ */
+function extract_data(deb_path, cb) {
+  var archive = new ar.Archive(fs.readFileSync(deb_path)),
+      files = archive.getFiles(), i, file,
+      found = false, tarFile = deb_path + ".tar", stream,
+      stream_finish_cb = function(err) {
+        // Close the stream before passing the callback.
+        stream.close();
+        cb(err);
+      };
+
+  // Iterate through the files to find data.tar.gz.
+  for (i = 0; i < files.length; i++) {
+    file = files[i];
+    if (file.name() === 'data.tar.gz') {
+      found = true;
+      break;
+    }
+  }
+
+  if (found) {
+    // Decompress the file.
+    zlib.gunzip(file.fileData(), function(err, buff) {
+      if (err) return cb(err);
+      // Write the tar file to disc so we can create a read stream for it.
+      // There's no built-in way to create a stream from a buffer in Node.
+      fs.writeFileSync(tarFile, buff);
+      // Extract the tar file.
+      stream = fs.createReadStream(tarFile);
+      stream.pipe(tar.Extract({ path: DOWNLOAD_DIR })).on("error", stream_finish_cb).on("end", stream_finish_cb);
     });
-  });
+  } else {
+    cb(new Error("Could not find data.tar.gz in " + deb_path + "."));
+  }
+}
+
+function jcl_download(cb) {
+  var finish_cb = function(err) {
+    process.chdir(RUN_DIR);
+    cb(err);
+  };
+
+  console.log('Downloading the Java class library (big download, may take a while)');
+  process.chdir(DOWNLOAD_DIR);
+  async.each(DEBS, function(deb, cb2){
+    var url = DEBS_DOMAIN + '/' + deb,
+        file = fs.createWriteStream(deb);
+    request(url).pipe(file);
+    file.on('finish', function() {
+      file.close();
+      extract_data(deb, cb2);
+    });
+  }, finish_cb);
 }
 
 function extract_jars(cb) {
-  console.log('Extracting JAR files');
   var jars = ["rt.jar", "tools.jar", "resources.jar", "rhino.jar", "jsse.jar"];
+  console.log('Extracting JAR files');
   async.each(jars, function(jar, cb2){
-    var cmd = 'find '+DOWNLOAD_DIR+'/usr -name '+jar+' | head -1';
+    // Locate jar file.
+    var cmd = 'find "' + path.resolve(DOWNLOAD_DIR, 'usr') + '" -name ' + jar + ' | head -1';
     exec(cmd, function(err, stdout, stderr){
       var jar_path = stdout.trim();
       console.log('Extracting the Java class library from ' + jar_path);
-      exec('unzip -qq -o -d classes/ "'+jar_path+'"', function(err, stdout, stderr){
+      exec('unzip -qq -o "'+jar_path+'" -x META_INF -d "' + CLASSES_DIR + '"', function(err, stdout, stderr){
         return cb2(err);
       });
     });
@@ -142,14 +190,15 @@ function extract_jars(cb) {
 }
 
 function symlink_java_home(cb) {
-  if (fs.existsSync('java_home')) {
+  var java_home = path.resolve(VENDOR_DIR, 'java_home'),
+      JH = path.resolve(DOWNLOAD_DIR, 'usr', 'lib', 'jvm', 'java-6-openjdk-i386', 'jre');
+  if (fs.existsSync(java_home)) {
     return cb(null);
   }
   console.log('Symlinking files into java_home');
-  var JH = DOWNLOAD_DIR+'/usr/lib/jvm/java-6-openjdk-i386/jre';
   // a number of .properties files are symlinks to /etc; copy the targets over
   // so we do not need to depend on /etc's existence
-  exec('find '+JH+' -type l', function(err, stdout, stderr){
+  exec('find "' + JH + '" -type l', function(err, stdout, stderr){
     if (err !== null) return cb(err);
     var links = stdout.split(/\r?\n/g);
     async.each(links, function(link, cb2){
@@ -181,7 +230,7 @@ function symlink_java_home(cb) {
       }
       cb2(null);
     }, function(err){
-      fs.renameSync(JH, 'java_home');
+      fs.renameSync(JH, java_home);
       return cb(err);
     });
   });
@@ -196,14 +245,7 @@ function jcl_setup(cb) {
     jcl_download,
     extract_jars,
     symlink_java_home
-  ], function(err){
-    if (err !== null) return cb(err);
-    var rimraf = require('rimraf');
-    rimraf(DOWNLOAD_DIR, function(err){
-      process.chdir('..');  // back out of vendor/
-      return cb(err);
-    });
-  });
+  ], cb);
 }
 
 function get_ecj(cb) {
@@ -213,77 +255,54 @@ function get_ecj(cb) {
      With Doppio: (see issue #218)
        ./doppio -Djdt.compiler.useSingleThread -jar vendor/jars/ecj.jar -1.6 classes/demo/Fib.java
   */
-  var ecj_pathname = 'vendor/jars/ecj.jar';
+  var jar_dir = path.resolve(VENDOR_DIR, 'jars'),
+      ecj_pathname = path.resolve(jar_dir, 'ecj.jar');
   if (fs.existsSync(ecj_pathname)) return cb(null);
   console.log('Downloading the ECJ compiler.');
   var ecj_url = "http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.7.1-201109091335/ecj-3.7.1.jar";
-  if (!fs.existsSync('vendor/jars')) fs.mkdirSync('vendor/jars');
+  if (!fs.existsSync(jar_dir)) fs.mkdirSync(jar_dir);
   var ecj_file = fs.createWriteStream(ecj_pathname);
-  var request = require('request');
   request(ecj_url).pipe(ecj_file);
   ecj_file.on('finish', function(){
     ecj_file.close();
     // TODO: use node-zip here, instead of exec
-    exec('unzip -qq -o -d vendor/classes/ '+ecj_pathname, function(err, stdout, stderr){
+    exec('unzip -qq -o "' + ecj_pathname + '" -x META_INF -d "' + CLASSES_DIR + '"', function(err, stdout, stderr){
       return cb(err);
     });
   });
 }
 
 function patch_jazzlib(cb) {
+  var jazzlib_dir = path.resolve(DOWNLOAD_DIR, 'jazzlib'),
+      jazzlib_zippath = path.resolve(jazzlib_dir, 'jazzlib.zip');
   // check for jazzlib
-  if (fs.existsSync('vendor/classes/java/util/zip/DeflaterEngine.class')) return cb(null);
+  if (fs.existsSync(path.resolve(CLASSES_DIR, 'java', 'util', 'zip', 'DeflaterEngine.class'))) return cb(null);
   console.log("patching the class library with Jazzlib");
-  if (!fs.existsSync('jazzlib')) fs.mkdirSync('jazzlib');
+  if (!fs.existsSync(jazzlib_dir)) fs.mkdirSync(jazzlib_dir);
   var url = "http://downloads.sourceforge.net/project/jazzlib/jazzlib/0.07/jazzlib-binary-0.07-juz.zip";
-  var jazzlib_file = fs.createWriteStream('jazzlib/jazzlib.zip');
-  var request = require('request');
+  var jazzlib_file = fs.createWriteStream(jazzlib_zippath);
   request(url).pipe(jazzlib_file);
   jazzlib_file.on('finish', function(){
     jazzlib_file.close();
     // TODO: use node-zip here, instead of exec
-    exec('unzip -qq jazzlib/jazzlib.zip -d jazzlib/', function(err, stdout, stderr){
+    exec('unzip -qq "' + jazzlib_zippath + '" -x META_INF -d "' + jazzlib_dir + '"', function(err, stdout, stderr){
+      var jazzlib_zip_dir = path.resolve(jazzlib_dir, 'java', 'util', 'zip'),
+          jcl_zip_dir = path.resolve(VENDOR_DIR, 'classes', 'java', 'util', 'zip');
       if (err!==null) return cb(err);
-      var zipfiles = fs.readdirSync('jazzlib/java/util/zip');
+      var zipfiles = fs.readdirSync(jazzlib_zip_dir);
       async.each(zipfiles, function(fname, cb2){
         if (!fname.match(/\.class$/)) return cb2(null);
-        fs.rename(path.join('jazzlib/java/util/zip', fname),
-                  path.join('vendor/classes/java/util/zip', fname),
+        fs.rename(path.join(jazzlib_zip_dir, fname),
+                  path.join(jcl_zip_dir, fname),
                   cb2);
-      }, function(err){
-        if (err!==null) return cb(err);
-        require('rimraf')('jazzlib', function(err){
-          cb(err);
-        });
-      });
+      }, cb);
     });
-  });
-}
-
-function update_npm_packages(cb) {
-  npm.load(function(err, npm){
-    if (err !== null) return cb(err);
-    console.log("Installing required node modules");
-    npm.install(function(err){
-      return cb(err);
-    });
-  });
-}
-
-function update_bower_packages(cb) {
-  console.log("Installing frontend dependencies");
-  require('bower').commands.install()
-  .on('end', function(){
-    cb(null);
-  })
-  .on('error', function(err){
-    cb(err);
   });
 }
 
 function check_node_version(cb) {
   console.log('Checking node version...');
-  if (require('semver').lt(process.versions.node, '0.10.0')) {
+  if (semver.lt(process.versions.node, '0.10.0')) {
     console.log('\tnode >= v0.10.0 required, please update.');
     cb(true);
   }
@@ -310,52 +329,24 @@ function make_java(cb) {
   });
 }
 
-function main() {
-  process.chdir(__dirname + '/..');
-  async.series([
-    find_java_home,
-    check_java_version,
-    update_npm_packages,
-    check_node_version,
-    update_bower_packages,
-    jcl_setup,
-    get_ecj,
-    patch_jazzlib,
-    make_java
-  ], function(err){
-    if (err!==null) {
-      console.error(err);
-    } else {
-      console.log('Your environment should now be set up correctly.');
-      console.log("Run 'make test' (optionally with -j4) to test Doppio.");
-    }
-  });
-}
-
-// Ensure that the async and npm modules are loaded.
-var needs_bootstrap = false;
-try {
-  async = require('async');
-  npm = require('npm');
-} catch (err) {
-  needs_bootstrap = true;
-  var cmd = 'npm install async npm';
-  if (process.platform.match(/CYGWIN/i)) {
-    cmd = 'cmd /c ' + cmd;
+process.chdir(RUN_DIR);
+// We randomly generated the download directory. It shouldn't exist.
+fs.mkdirSync(DOWNLOAD_DIR);
+async.series([
+  find_java_home,
+  check_java_version,
+  check_node_version,
+  jcl_setup,
+  get_ecj,
+  patch_jazzlib,
+  make_java,
+  // Delete the temporary download directory.
+  function(cb) { rimraf(DOWNLOAD_DIR, cb); }
+], function(err) {
+  if (err !== null) {
+    console.error(err);
+  } else {
+    console.log('Your environment should now be set up correctly.');
+    console.log("Run 'make test' (optionally with -j4) to test Doppio.");
   }
-  console.log('Bootstrapping required modules: async, npm');
-  exec(cmd, function(err, stdout, stderr){
-    if (err!==null) {
-      console.error(err);
-      console.error("Couldn't install required npm modules: async, npm");
-      process.exit(1);
-    } else {
-      async = require('async');
-      npm = require('npm');
-      main();
-    }
-  });
-}
-if (!needs_bootstrap) {
-  main();
-}
+});
