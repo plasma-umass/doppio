319ad576930be45af7374a4c03f2f27e76097735
John Vilk <jvilk@cs.umass.edu>
2014-04-30 15:12:34 -0400

    Changing exception to use external class name.

--- src/ClassLoader.ts
+++ src/ClassLoader.ts
@@ -373,7 +373,7 @@ export class ClassLoader {
    * If the program explicitly called loadClass, then we throw the ClassNotFoundException.
    */
   public throwClassNotFoundException(thread: threading.JVMThread, typeStr: string, explicit: boolean): void {
-    thread.throwNewException(explicit ? 'Ljava/lang/ClassNotFoundException;' : 'Ljava/lang/NoClassDefFoundError;', 'Could not load class: ' + typeStr);
+    thread.throwNewException(explicit ? 'Ljava/lang/ClassNotFoundException;' : 'Ljava/lang/NoClassDefFoundError;', 'Could not load class: ' + util.ext_classname(typeStr));
   }
 
   /**
