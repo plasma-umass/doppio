6251988c4aa40a07ab90d37c1557172b020768b8
CJ Carey <perimosocordiae@gmail.com>
2015-01-15 12:08:59 -0500

    Adding shutdown hook test case
    Minimal example for #348

--- classes/test/Runtime.java
+++ classes/test/Runtime.java
@@ -7,5 +7,12 @@ public class Runtime {
     long maxMemory = rt.maxMemory();
 
     System.out.println(maxMemory > 0L);
+
+    rt.addShutdownHook(new Thread() {
+    	public void run() {
+    		System.out.println("shutdown hook");
+    	}
+    });
+    System.out.println("Exiting...");
   }
 }
