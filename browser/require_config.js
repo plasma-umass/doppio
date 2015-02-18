30fcb55ad9e44e25856fbd1a070f980adb4242b4
John Vilk <jvilk@cs.umass.edu>
2015-02-17 23:21:00 -0500

    Removing jquery/underscore mentions in R.JS config.

--- browser/require_config.js
+++ browser/require_config.js
@@ -1,18 +1,4 @@
 require.config({
-  shim: {
-    'vendor/underscore/underscore': {
-      exports: '_'
-    },
-    'vendor/jquery/dist/jquery.min': {
-      exports: '$'
-    },
-    'vendor/jquery-migrate/jquery-migrate.min': {
-      deps: ['vendor/jquery/dist/jquery.min']
-    },
-    'vendor/jquery.console': {
-      deps: ['vendor/jquery/dist/jquery.min']
-    }
-  },
   paths: {
     fs: 'browser/fs',
     path: 'browser/path'
