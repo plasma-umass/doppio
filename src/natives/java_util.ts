b3dbc4e4314da2c9a030f94dafe65ede93ee1667
Kenneth Cochran <kenneth.cochran101@gmail.com>
2016-02-06 15:33:06 -0600

    added natives for zipping

--- src/natives/java_util.ts
+++ src/natives/java_util.ts
@@ -308,34 +308,141 @@ class java_util_zip_CRC32 {
 class java_util_zip_Deflater {
 
   public static 'initIDs()V'(thread: JVMThread): void {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
+    thread.getJVMObject
   }
 
-  public static 'init(IIZ)J'(thread: JVMThread, arg0: number, arg1: number, arg2: number): Long {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
-    return null;
+  /**
+   * Initialize a new deflater. Using the zlib recommended default values.
+   */
+
+  public static 'init(IIZ)J'(thread: JVMThread, level: number, strategy: number, nowrap: number): Long {
+    let DEF_MEM_LEVEL = 8; // Zlib recommended default
+    let Z_DEFLATED = 8;    // This value is in the js version of pako under pako.Z_DEFLATED. 
+    // Possibly it is set to private in the Typescript version. The default value is 8, so this should work fine
+
+    let strm = new ZStreamCons();
+    let ret = deflate.deflateInit2(strm, level, Z_DEFLATED, nowrap ? -MAX_WBITS : MAX_WBITS, DEF_MEM_LEVEL, strategy);
+    
+    if (ret != ZlibReturnCode.Z_OK) {
+      let msg = ((strm.msg != null) ? strm.msg :
+                (ret == ZlibReturnCode.Z_STREAM_ERROR) ?
+                "inflateInit2 returned Z_STREAM_ERROR" :
+                "unknown error initializing zlib library");
+      thread.throwNewException("Ljava/lang/InternalError;", msg);
+    }
+    let num = OpenZStream(strm);
+    return Long.fromNumber(num);
   }
+
+  /**
+   * Apparently this is explicitly not supported by pako.
+   * @see Notes at http://nodeca.github.io/pako/
+   */
 
   public static 'setDictionary(J[BII)V'(thread: JVMThread, arg0: Long, arg1: JVMTypes.JVMArray<number>, arg2: number, arg3: number): void {
     thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
   }
 
-  public static 'deflateBytes(J[BIII)I'(thread: JVMThread, javaThis: JVMTypes.java_util_zip_Deflater, arg0: Long, arg1: JVMTypes.JVMArray<number>, arg2: number, arg3: number, arg4: number): number {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
+  public static 'deflateBytes(J[BIII)I'(thread: JVMThread, javaThis: JVMTypes.java_util_zip_Deflater, addr: Long, b: JVMTypes.JVMArray<number>, off: number, len: number, flush: number): number {
+    let strm = GetZStream(thread, addr.toNumber());
+    if (!strm) return;
+
+    let thisBuf = javaThis['java/util/zip/Deflater/buf'];
+    let thisOff = javaThis['java/util/zip/Deflater/off'];
+    let thisLen = javaThis['java/util/zip/Deflater/len'];
+
+    let res: Number;
+
+    if (javaThis['java/util/zip/Deflater/setParams']) {
+      let level = javaThis['java/util/zip/Deflater/level'];
+      let strategy = javaThis['java/util/zip/Deflater/level'];
+
+      let inBuf = thisBuf.array;
+      let outBuf = b.array;
+
+      strm.input = i82u8(inBuf, 0, inBuf.length);
+      strm.next_in = thisOff;
+      strm.avail_in = thisLen;
+
+      strm.output = i82u8(outBuf, 0, outBuf.length);
+      strm.next_out = off;
+      strm.avail_out = len;
+      
+      //deflateParams is not yet supported by pako. We'll open a new ZStream with the new parameters instead.
+      // res = deflate.deflateParams(strm, level, strategy); 
+      let newStream = new ZStreamCons();
+      res = deflate.deflateInit2(newStream, strm.state.level, strm.state.method, strm.state.windowBits, strm.state.memLevel, strm.state.strategy);
+      ZStreams[addr.toNumber()] = newStream;
+      switch (res) {
+        case ZlibReturnCode.Z_OK:
+          javaThis['java/util/zip/Deflater/setParams'] = 0;
+          thisOff += thisLen - strm.avail_in;
+          javaThis['java/util/zip/Deflater/off'] = thisOff;
+          javaThis['java/util/zip/Deflater/len'] = strm.avail_in;
+          return len - strm.avail_out;
+        case ZlibReturnCode.Z_BUF_ERROR:
+          javaThis['java/util/zip/Deflater/setParams'] = 0;
+          return 0;
+        default:
+          thread.throwNewException("Ljava/lang/InternalError;", strm.msg);
+      }
+    } else {
+      let finish = javaThis['java/util/zip/Deflater/finish'];
+      let inBuf = thisBuf.array;
+      let outBuf = b.array;
+
+      strm.input = i82u8(inBuf, 0, inBuf.length);
+      strm.next_in = thisOff;
+      strm.avail_in = thisLen;
+
+      strm.output = i82u8(outBuf, 0, outBuf.length);
+      strm.next_out = off;
+      strm.avail_out = len;
+      res = deflate.deflate(strm, finish ? ZlibFlushValue.Z_FINISH : flush);
+
+      switch (res) {
+        case ZlibReturnCode.Z_STREAM_END:
+          javaThis['java/util/zip/Deflater/finished'] = 1;
+          // intentionally fall through
+        case ZlibReturnCode.Z_OK:
+          thisOff += thisLen - strm.avail_in;
+          javaThis['java/util/zip/Deflater/off'] = thisOff;
+          javaThis['java/util/zip/Deflater/len'] = strm.avail_in;
+          return len - strm.avail_out;
+        case ZlibReturnCode.Z_BUF_ERROR:
+          return 0;
+        default:
+          thread.throwNewException('Ljava/lang/InternalError;', strm.msg);
+      }
+    }
     return 0;
   }
 
-  public static 'getAdler(J)I'(thread: JVMThread, arg0: Long): number {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
-    return 0;
+  public static 'getAdler(J)I'(thread: JVMThread, addr: Long): number {
+    let strm = GetZStream(thread, addr.toNumber());
+    if (strm) {
+      return strm.adler;
+    }
   }
 
-  public static 'reset(J)V'(thread: JVMThread, arg0: Long): void {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
+  public static 'reset(J)V'(thread: JVMThread, addr: Long): void {
+    let strm = GetZStream(thread, addr.toNumber());
+    if (strm) {
+      if (deflate.deflateReset(strm) != ZlibReturnCode.Z_OK) {
+        thread.throwNewException('Ljava/lang/InternalError;', strm.msg);
+      }
+    }
   }
 
-  public static 'end(J)V'(thread: JVMThread, arg0: Long): void {
-    thread.throwNewException('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
+  public static 'end(J)V'(thread: JVMThread, addr: Long): void {
+    let strm = GetZStream(thread, addr.toNumber());
+    if (strm) {
+      if (deflate.deflateEnd(strm) == ZlibReturnCode.Z_STREAM_ERROR) {
+        thread.throwNewException('Ljava/lang/InternalError;', strm.msg);
+      } else {
+        CloseZStream(addr.toNumber());
+      }
+    }
   }
 
 }
